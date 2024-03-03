6fcda4a4c6c96823c77a130f3fc83da1e7506955
PracticeEveryday <developerforus@gmail.com>
2024-03-03 17:59:57 +0900

    feat: add success test

--- src/api/user/user.controller.spec.ts
+++ src/api/user/user.controller.spec.ts
@@ -1,13 +1,14 @@
-// user.controller.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserEntity } from '../../libs/db/typeorm/entities/user.entity';
 import { UserProviders } from '../../libs/db/typeorm/providers/user.providers';
+import { CreateUserReqDto, CreateUserResDto } from './dtos/create.dto';
+import { UserDomain } from './domain/user.domain';
 
 const mockUserRepository = {
-  crete: jest.fn(),
+  save: jest.fn(),
 };
 
 describe('UserController', () => {
@@ -33,5 +34,25 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Create 생성 성공 시 요청과 동일한 값을 반환한다.', async () => {
+    // Arrange
+    const createUserReqDto = new CreateUserReqDto('test@example.com', 'kim');
+    const userDomain = new UserDomain('test@example.com', 'kim');
+    const createUserResDto = new CreateUserResDto(userDomain);
+
+    jest.spyOn(userService, 'create').mockResolvedValueOnce(createUserResDto);
+    const result = await controller.create(createUserReqDto);
+
+    expect(result).toEqual(createUserResDto);
+    expect(userService.create).toHaveBeenCalledWith(createUserReqDto);
+  });
+
+  // 목데이터이기 떄문에 생성된 UserEntity가 없어 Error가 반환된다.
+  it('email이 없으면 Not Found Error를 반환한다.', async () => {
+    await expect(async () => {
+      await controller.create({ name: 'kim', email: 'hello' });
+    }).rejects.toThrowError(new Error('Not Found UserEntity'));
   });
 });
